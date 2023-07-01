11b88474dd703a260bde0cdd7a868701cec1246f
Sicelo Mgobhozi <scelomgobhozi27@gmail.com>
2023-07-01 19:19:42 +0200

    models with relationships untested

---
+++ app/Models/participants.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class participants extends Model
+{
+    use HasFactory;
+    public function User() :BelongsTo
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function Rooms():BelongsTo
+    {
+        return $this->belongsTo('App\Rooms');
+    }
+}
